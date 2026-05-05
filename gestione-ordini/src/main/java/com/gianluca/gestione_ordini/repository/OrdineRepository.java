d5426746d189d019919dcb82ed0116e56380c665
uchiha10 <exp20@hotmail.it>
2026-05-05 20:38:23 +0200

    Sincronizzazione definitiva file locali e remoti

--- gestione-ordini/src/main/java/com/gianluca/gestione_ordini/repository/OrdineRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gianluca.gestione_ordini.repository;
-
-import com.gianluca.gestione_ordini.model.Ordine;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrdineRepository extends JpaRepository<Ordine, Long> {
-}
